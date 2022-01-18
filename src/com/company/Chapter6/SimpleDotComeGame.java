9c22b0c673a1767166564572ddefa0ae2e3f6932
Rostyslav Andreev <rostyslav.andreev@gmail.com>
2022-01-18 16:01:27 +0200

    Chapter6 real code

---
+++ src/com/company/Chapter6/SimpleDotComeGame.java
@@ -0,0 +1,22 @@
+package com.company.Chapter6;
+
+public class SimpleDotComeGame {
+    public static void main(String[] args) {
+        int numOfGuesses = 0;
+        GameHelper helper = new GameHelper();
+        SimpleDotCom theDotCome = new SimpleDotCom();
+        int random = (int) (Math.random()*5);
+        int [] locations = {random, random++, random+2};
+        theDotCome.setLocationCells(locations);
+        boolean isAlive = true;
+        while (isAlive == true){
+            String guess = helper.getUserInput("enter a number");
+            String result = theDotCome.checkYourself(guess);
+            numOfGuesses++;
+            if(result.equals("kill")){
+                isAlive = false;
+                System.out.println("You took " + numOfGuesses + "guesses" );
+            }
+        }
+    }
+}
